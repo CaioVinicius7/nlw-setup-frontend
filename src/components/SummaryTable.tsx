0ba9e7f2d3efe3cee851b77f1a8d427bebb0f3ab
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-29 14:25:19 -0300

    feat: :lipstick: Preenchimento minímo de cards de hábito

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -6,6 +6,10 @@ const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearBeginning();
 
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
 export function SummaryTable() {
 	return (
 		<div className="w-full flex">
@@ -24,6 +28,16 @@ export function SummaryTable() {
 				{summaryDates.map((date) => (
 					<HabitDay key={date.toString()} />
 				))}
+
+				{amountOfDaysToFill > 0 &&
+					Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+						return (
+							<div
+								key={i}
+								className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+							/>
+						);
+					})}
 			</div>
 		</div>
 	);
