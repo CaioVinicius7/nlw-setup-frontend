44b9d6e0ed60f44d2a5a7faa6ca20b395aec2571
Caio Vinícius <caio1525pereira@gmail.com>
2023-01-28 21:47:33 -0300

    feat: :lipstick: Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Header } from "./components/Header";
+
 import "./styles/global.css";
 
 export function App() {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
-			<main className="w-full max-w-5xl px-6 flex flex-col gap-16"></main>
+			<main className="w-full max-w-5xl px-6 flex flex-col gap-16">
+				<Header />
+			</main>
 		</div>
 	);
 }
