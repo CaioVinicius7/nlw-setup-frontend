6ee2ff230afd606848ab8d3c4c61b98e79cc1ade
Caio Vinícius <caio1525pereira@gmail.com>
2023-02-08 20:57:48 -0300

    feat: :wrench: Configuração de localidade do dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 
+import "./lib/dayjs.ts";
+
 import "./styles/global.css";
 
 export function App() {
