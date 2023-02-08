6a2c13e7819b7f4f557e641cf9f7480bdbd4306f
Caio Vinícius <caio1525pereira@gmail.com>
2023-02-08 20:26:52 -0300

    feat: :sparkles: Adição das propiedades do checkbox do Radix ao componente Checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,17 +1,18 @@
 import clsx from "clsx";
 import { Check } from "phosphor-react";
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
+import type { CheckboxProps as RadixCheckboxProps } from "@radix-ui/react-checkbox";
 
-interface CheckboxProps {
+interface CheckboxProps extends RadixCheckboxProps {
 	title: string;
 	variant: "popover" | "modal";
 }
 
-export function Checkbox({ title, variant }: CheckboxProps) {
+export function Checkbox({ title, variant, ...props }: CheckboxProps) {
 	const isPopoverVariant = variant === "popover";
 
 	return (
-		<RadixCheckbox.Root className="flex items-center gap-3 group">
+		<RadixCheckbox.Root className="flex items-center gap-3 group" {...props}>
 			<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
 				<RadixCheckbox.Indicator>
 					<Check size={20} className="text-white" />
