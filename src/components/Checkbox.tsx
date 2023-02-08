00f116525decbb167fd81d2a1fc94647ba12daf5
Caio Vinícius <caio1525pereira@gmail.com>
2023-02-08 20:03:36 -0300

    feat: :sparkles: Componente Checkbox

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+import { Check } from "phosphor-react";
+import * as RadixCheckbox from "@radix-ui/react-checkbox";
+
+interface CheckboxProps {
+	title: string;
+	variant: "popover" | "modal";
+}
+
+export function Checkbox({ title, variant }: CheckboxProps) {
+	const isPopoverVariant = variant === "popover";
+
+	return (
+		<RadixCheckbox.Root className="flex items-center gap-3 group">
+			<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+				<RadixCheckbox.Indicator>
+					<Check size={20} className="text-white" />
+				</RadixCheckbox.Indicator>
+			</div>
+
+			<span
+				className={clsx(" text-white leading-tight", {
+					"font-semibold text-xl group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400":
+						isPopoverVariant
+				})}
+			>
+				{title}
+			</span>
+		</RadixCheckbox.Root>
+	);
+}
